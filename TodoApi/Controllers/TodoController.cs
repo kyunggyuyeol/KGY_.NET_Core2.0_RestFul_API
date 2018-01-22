6ce31708a395c3a0f90bd70e53d246cc649f0cfb
경규열 <p82468@paran.com>
2018-01-22 22:22:09 +0900

    ZXcvasfdg

--- TodoApi/Controllers/TodoController.cs
+++ TodoApi/Controllers/TodoController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -20,9 +21,16 @@ namespace TodoApi.Controllers
 
         // GET api/<controller>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public List<TodoItem> Get(int id)
         {
-            return "value";
+            List<TodoItem> returnValue = new List<TodoItem>();
+            returnValue.Add(new TodoItem() { Id = 1, Name = "a", IsComplete = true });
+            returnValue.Add(new TodoItem() { Id = 2, Name = "B", IsComplete = false });
+            returnValue.Add(new TodoItem() { Id = 3, Name = "C", IsComplete = true });
+            returnValue.Add(new TodoItem() { Id = 4, Name = "D", IsComplete = false });
+            returnValue.Add(new TodoItem() { Id = 5, Name = "E", IsComplete = true });
+
+            return returnValue;
         }
 
         // POST api/<controller>
